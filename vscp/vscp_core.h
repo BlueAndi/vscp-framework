60ce2f0212e8123cedec20e5c466f006b7dd7cd8
BlueAndi <vscp@blue-andi.de>
2015-01-25 20:47:50 +0100

    VSCP framework bugfix version v0.1.1

--- vscp/vscp_core.h
+++ vscp/vscp_core.h
@@ -105,7 +105,7 @@ extern "C"
 #define VSCP_CORE_VERSION_STR       "v1.10.12"
 
 /** VSCP framework version string */
-#define VSCP_CORE_FRAMEWORK_VERSION "v0.1.0"
+#define VSCP_CORE_FRAMEWORK_VERSION "v0.1.1"
 
 /*******************************************************************************
     MACROS
