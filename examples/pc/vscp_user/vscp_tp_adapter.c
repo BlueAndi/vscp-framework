672d30c9e73b6d73081a6384868020f7111eb560
BlueAndi <web@blue-andi.de>
2015-05-19 22:28:52 +0200

    printf format fixed and event message output shorten

--- examples/pc/vscp_user/vscp_tp_adapter.c
+++ examples/pc/vscp_user/vscp_tp_adapter.c
@@ -562,7 +562,7 @@ extern VSCP_TP_ADAPTER_RET vscp_tp_adapter_connect(char const * const ipAddr, ch
         }
         else
         {
-            log_printf("Used VSCP dll version 0x%08X\n", dllVersion);
+            log_printf("Used VSCP dll version 0x%08lX\n", dllVersion);
         }
         
         /* Get vendor from driver */
@@ -750,7 +750,7 @@ static void vscp_tp_adapter_showMessage(vscp_Message const * const msg, BOOL isR
 {
     uint8_t index = 0;
 
-    log_printf("%cx: class=0x%02X, type=0x%02X, prio=%2d, oAddr=0x%02X, %c, num=%u, data=",
+    log_printf("%cx: class=0x%02X type=0x%02X prio=%2d oAddr=0x%02X %c num=%u data=",
         (TRUE == isReceived) ? 'R' : 'T',
         msg->vscpClass,
         msg->vscpType,
