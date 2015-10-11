12737f5564d3ae3e2d7733560e9331bf1162ec27
BlueAndi <web@blue-andi.de>
2015-10-11 15:25:24 +0200

    Subversion keywords removed

--- common/pc/cmdLineParser.h
+++ common/pc/cmdLineParser.h
@@ -35,10 +35,6 @@
 @section desc Description
 This module provides a command line parser.
 
-@section svn Subversion
-$Author: amerkle $
-$Rev: 449 $
-$Date: 2015-01-05 20:23:52 +0100 (Mo, 05 Jan 2015) $
 *******************************************************************************/
 /** @defgroup cmdLineParser Command line parser
  * Functionality of a command line parser.
