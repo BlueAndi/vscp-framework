22f9702386220baec5dfd3f2c8cd7edb7da15370
troky <troky2001@yahoo.com>
2018-11-07 08:37:06 +0100

    Custom heartbeat event

--- vscp/templates/vscp_portable.c
+++ vscp/templates/vscp_portable.c
@@ -41,6 +41,7 @@
     INCLUDES
 *******************************************************************************/
 #include "vscp_portable.h"
+#include "vscp_dev_data.h"
 #include "vscp_core.h"
 
 /*******************************************************************************
@@ -271,6 +272,20 @@ extern uint8_t  vscp_portable_readGUID(uint8_t index)
 
     return value;
 }
+
+#if VSCP_CONFIG_BASE_IS_ENABLED( VSCP_CONFIG_ENABLE_CUSTOM_HEARTBEAT )
+
+/**
+ * Custom node heartbeat event function implementation.
+ */
+extern BOOL vscp_portable_sendNodeHeartbeatEvent() {
+   
+  /* Implement your version of vscp_information_sendNodeHeartbeatEvent() */
+  
+  return TRUE;
+}
+
+#endif  /* VSCP_CONFIG_BASE_IS_ENABLED( VSCP_CONFIG_ENABLE_CUSTOM_HEARTBEAT ) */
 
 #endif  /* VSCP_CONFIG_BASE_IS_ENABLED( VSCP_DEV_DATA_CONFIG_ENABLE_GUID_STORAGE_EXT ) */
 
--- vscp/templates/vscp_portable.h
+++ vscp/templates/vscp_portable.h
@@ -209,6 +209,15 @@ extern uint8_t  vscp_portable_readGUID(uint8_t index);
 
 #endif  /* VSCP_CONFIG_BASE_IS_ENABLED( VSCP_DEV_DATA_CONFIG_ENABLE_GUID_STORAGE_EXT ) */
 
+#if VSCP_CONFIG_BASE_IS_ENABLED( VSCP_CONFIG_ENABLE_CUSTOM_HEARTBEAT )
+
+/**
+ * Custom node heartbeat event function implementation.
+ */
+extern BOOL vscp_portable_sendNodeHeartbeatEvent();
+
+#endif  /* VSCP_CONFIG_BASE_IS_ENABLED( VSCP_CONFIG_ENABLE_CUSTOM_HEARTBEAT ) */
+
 #ifdef __cplusplus
 }
 #endif
