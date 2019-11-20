bf69b9d332c2d8851c1da7b93e2e78e475f816b1
BlueAndi <vscp@blue-andi.de>
2019-11-20 21:17:24 +0100

    Convert line endings to windows format and replace tabs with spaces for generated files.

--- projects/avr/relay_board_01/generator/generate_app_register.bat
+++ projects/avr/relay_board_01/generator/generate_app_register.bat
@@ -4,6 +4,10 @@ rem ****************************************************************************
 rem Author: Andreas Merkle, http://www.blue-andi.de
 rem ********************************************************************************
 
+rem GNU32 tools
+set SED=..\..\..\..\tools\gnu32\sed.exe
+set EXPAND=..\..\..\..\tools\gnu32\expand.exe
+
 echo Generate application register access ...
 java -jar ../../../../tools/xslt/saxon/saxon9he.jar -s:app_register.xml -xsl:xml2appReg.xsl
 if not %ERRORLEVEL% == 0 (
@@ -16,6 +20,23 @@ if not %ERRORLEVEL% == 0 (
 pause
 )
 
+rem Convert unix to windows line endings
+echo Convert line endings ...
+%SED% -i "s/$//" *.h
+%SED% -i "s/$//" *.c
+
+rem Convert tabs to spaces
+echo Convert tabs to spaces ...
+for /f "delims=" %%a in ('dir /b /a-d "*.c" "*.h"') do (
+    %EXPAND% -t 4 %%a > temp.tmp
+    type temp.tmp > %%a
+)
+
+rem Remove temporary files
+del temp.tmp
+del /Q sed*
+
+rem Copy generated files to project
 copy /Y vscp_app_reg.c ..\vscp_user
 copy /Y vscp_ps_user.* ..\vscp_user
 
--- tools/scripts/generate_vscp_type_headers.bat
+++ tools/scripts/generate_vscp_type_headers.bat
@@ -4,8 +4,32 @@ rem ****************************************************************************
 rem Author: Andreas Merkle, http://www.blue-andi.de
 rem ********************************************************************************
 
+rem GNU32 tools
+set SED=..\gnu32\sed.exe
+set EXPAND=..\gnu32\expand.exe
+
 echo Generate VSCP type headers ...
 java -jar ../xslt/saxon/saxon9he.jar -s:../vscp-protocol/vscp-protocol.xml -xsl:vscp-protocol2vscp-type-headers.xsl
 if not %ERRORLEVEL% == 0 (
 pause
 )
+
+rem Convert unix to windows line endings
+echo Convert line endings ...
+%SED% -i "s/$//" *.h
+
+rem Convert tabs to spaces
+echo Convert tabs to spaces ...
+for /f "delims=" %%a in ('dir /b /a-d "*.h"') do (
+    %EXPAND% -t 4 %%a > temp.tmp
+    type temp.tmp > %%a
+)
+
+rem Remove temporary files
+del temp.tmp
+del /Q sed*
+
+rem Copy generated files to project
+copy /Y vscp_type*.h ..\..\vscp
+
+del /Q vscp_type*.h
